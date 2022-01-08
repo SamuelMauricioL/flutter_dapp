9da7d1fa45e414c0ecf26d96910b969ebeda8894
SamuelMauricioL <67238016+SamuelMauricioL@users.noreply.github.com>
2022-01-07 20:22:08 -0500

    test to profile package ready #2

--- packages/profile/test/src/profile_test.dart
+++ packages/profile/test/src/profile_test.dart
@@ -1,11 +1,21 @@
 // ignore_for_file: prefer_const_constructors
-import 'package:flutter_test/flutter_test.dart';
+
 import 'package:profile/profile.dart';
+import 'package:test/test.dart';
 
 void main() {
   group('Profile', () {
     test('can be instantiated', () {
-      expect(Profile(), isNotNull);
+      expect(
+        User(email: '', password: '', name: '', dni: '', number: ''),
+        isNotNull,
+      );
+    });
+    test('Supports value equality', () {
+      expect(
+        User.empty(),
+        User.empty(),
+      );
     });
   });
 }
